f5ae9b9e3d9057398620d3a733886a60afe00150
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2021-10-26 21:13:08 -0500

    Update website to annouce SpaceX Crew-3 mission

--- assets/static/js/custom-scripts.js
+++ assets/static/js/custom-scripts.js
@@ -21,8 +21,8 @@ https://opensource.org/licenses/MIT
     //
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
-        missionName: "SpaceX NASA Crew-2",
-        launchAt: 1619171340, // the UNIX timestamp of the projected T-0 time
+        missionName: "SpaceX NASA Crew-3",
+        launchAt: 1635661260, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: true,
     };
     // const missionOverride = null;
