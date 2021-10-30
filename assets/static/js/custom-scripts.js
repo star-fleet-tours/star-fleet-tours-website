dd061f15945ca79f9a74b0368bddeef136b7b4fb
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2021-10-30 10:13:45 -0500

    Update site for Crew-3 72 hour delay

--- assets/static/js/custom-scripts.js
+++ assets/static/js/custom-scripts.js
@@ -22,7 +22,7 @@ https://opensource.org/licenses/MIT
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
         missionName: "SpaceX NASA Crew-3",
-        launchAt: 1635661260, // the UNIX timestamp of the projected T-0 time
+        launchAt: 1635916200, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: true,
     };
     // const missionOverride = null;
