bf3bd1c179b13c0026af4e1fa9b7bee98bed88e2
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2022-08-31 13:49:55 -0500

    Update site for take 2 of Artemis 1 launch viewing trip

--- assets/static/js/custom-scripts.js
+++ assets/static/js/custom-scripts.js
@@ -22,7 +22,7 @@ https://opensource.org/licenses/MIT
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
         missionName: "NASA SLS Artemis 1 Launch",
-        launchAt: 1661776380, // the UNIX timestamp of the projected T-0 time
+        launchAt: 1662221820, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: true,
     };
     // const missionOverride = null;
