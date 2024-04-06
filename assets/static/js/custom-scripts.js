80214cb3e593a558eb541f7ba8a9748aefab92d0
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2024-04-05 23:11:36 -0500

    Update site for second launch attempt of final Delta IV / NROL-70

--- assets/static/js/custom-scripts.js
+++ assets/static/js/custom-scripts.js
@@ -22,7 +22,7 @@ https://opensource.org/licenses/MIT
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
         missionName: "Ultimate Delta IV Heavy NROL-70",
-        launchAt: 1711647600, // the UNIX timestamp of the projected T-0 time
+        launchAt: 1712681580, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: true,
     };
     // const missionOverride = null;
