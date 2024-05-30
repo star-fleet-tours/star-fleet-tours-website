f4d800fc7aa9569a709619d0f584aa7348471b45
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2024-05-29 21:29:34 -0500

    Make minimal changes nessesary to update site for Star Fleet IFT-4

--- assets/static/js/custom-scripts.js
+++ assets/static/js/custom-scripts.js
@@ -21,8 +21,8 @@ https://opensource.org/licenses/MIT
     //
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
-        missionName: "SpaceX Starship IFT-3",
-        launchAt: 1710417600, // the UNIX timestamp of the projected T-0 time
+        missionName: "SpaceX Starship Flight 4",
+        launchAt: 1717588800, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: true,
     };
     // const missionOverride = null;
