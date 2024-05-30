19e80eabadaf519e860ebcaf1d183c9d37d109bb
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2024-05-29 21:55:33 -0500

    Add additional changes for Starship Flight 4 that Steven requested

--- assets/static/js/custom-scripts.js
+++ assets/static/js/custom-scripts.js
@@ -21,7 +21,7 @@ https://opensource.org/licenses/MIT
     //
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
-        missionName: "SpaceX Starship Flight 4",
+        missionName: "SpaceX Starship Test Flight 4",
         launchAt: 1717588800, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: true,
     };
