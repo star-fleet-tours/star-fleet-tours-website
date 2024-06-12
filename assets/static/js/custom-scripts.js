5147cede9901ab385a84eb0519074703882a23ec
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2024-06-12 10:25:44 -0400

    Add initial info for Falcon Heavy GOES-U mission (#49)

--- assets/static/js/custom-scripts.js
+++ assets/static/js/custom-scripts.js
@@ -21,8 +21,8 @@ https://opensource.org/licenses/MIT
     //
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
-        missionName: "SpaceX Starship Test Flight 4",
-        launchAt: 1717675200, // the UNIX timestamp of the projected T-0 time
+        missionName: "SpaceX Falcon Heavy GOES-U",
+        launchAt: 1719350160, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: true,
     };
     // const missionOverride = null;
