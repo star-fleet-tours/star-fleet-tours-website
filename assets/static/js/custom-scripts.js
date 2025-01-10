871c4f52bc3c24e6dd4e5c506a2844013ad0bc0d
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2025-01-09 19:36:30 -0600

    Update site for Jan 13 IFT7 date and slim down text

--- assets/static/js/custom-scripts.js
+++ assets/static/js/custom-scripts.js
@@ -22,7 +22,7 @@ https://opensource.org/licenses/MIT
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
         missionName: "SpaceX Starship Flight 7",
-        launchAt: 1736600400, // the UNIX timestamp of the projected T-0 time
+        launchAt: 1736805600, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: true,
     };
     // const missionOverride = null;
