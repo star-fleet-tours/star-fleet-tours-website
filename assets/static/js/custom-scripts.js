0605b2d92769019b0643a54adb0d9166155f5a96
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2025-03-01 01:27:31 -0600

    Update IFT8 launch date to March 3

--- assets/static/js/custom-scripts.js
+++ assets/static/js/custom-scripts.js
@@ -21,8 +21,8 @@ https://opensource.org/licenses/MIT
     //
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
-        missionName: "SpaceX Starship Flight 7",
-        launchAt: 1737064800, // the UNIX timestamp of the projected T-0 time
+        missionName: "SpaceX Starship Flight 8",
+        launchAt: 1741044600, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: true,
     };
     // const missionOverride = null;
