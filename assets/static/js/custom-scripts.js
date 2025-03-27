57a5b6dfbee03ce468a2fadbad71adcb4eb63e88
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2025-03-26 23:57:58 -0500

    Update Star Fleet site to standby page for Starship Flight 9

--- assets/static/js/custom-scripts.js
+++ assets/static/js/custom-scripts.js
@@ -21,7 +21,7 @@ https://opensource.org/licenses/MIT
     //
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
-        missionName: "SpaceX Starship Flight 8",
+        missionName: "SpaceX Starship Flight 9",
         launchAt: 1741303800, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: true,
     };
