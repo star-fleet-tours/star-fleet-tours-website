7ba70d2b791bf9ada2722edd36ddd6c5d38e44e1
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2025-05-24 03:50:53 -0500

    Update site with booking info for Starship Flight 9 (#79)

--- assets/static/js/custom-scripts.js
+++ assets/static/js/custom-scripts.js
@@ -22,7 +22,7 @@ https://opensource.org/licenses/MIT
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
         missionName: "SpaceX Starship Flight 9",
-        launchAt: 1741303800, // the UNIX timestamp of the projected T-0 time
+        launchAt: 1748388600, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: true,
     };
     // const missionOverride = null;
