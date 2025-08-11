806fa427f438a47129e75e451579d397ad2420db
CAM-Gerlach <CAM.Gerlach@Gerlach.CAM>
2025-08-11 20:41:20 +0000

    Deploying to gh-pages from @ star-fleet-tours/star-fleet-tours-website@a13dac8065ac7337613a2197715e22bed38b694f 🚀

--- static/js/custom-scripts.js
+++ static/js/custom-scripts.js
@@ -21,8 +21,8 @@ https://opensource.org/licenses/MIT
     //
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
-        missionName: "SpaceX Starship Flight 9",
-        launchAt: 1748388600, // the UNIX timestamp of the projected T-0 time
+        missionName: "SpaceX Starship Flight 10",
+        launchAt: 1755905400, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: true,
     };
     // const missionOverride = null;
