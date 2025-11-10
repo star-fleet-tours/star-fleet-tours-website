fdd8e1671e88624ac948c787dfb101e72ee9077d
CAM-Gerlach <CAM.Gerlach@Gerlach.CAM>
2025-11-10 01:09:19 +0000

    Deploying to gh-pages from @ star-fleet-tours/star-fleet-tours-website@f428e6f6330c78b06a50325a87149c211e27eb0f 🚀

--- static/js/custom-scripts.js
+++ static/js/custom-scripts.js
@@ -22,7 +22,7 @@ https://opensource.org/licenses/MIT
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
         missionName: "New Glenn NASA ESCAPADE",
-        launchAt: 1762717500, // the UNIX timestamp of the projected T-0 time
+        launchAt: 1762977000, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: true,
     };
     // const missionOverride = null;
