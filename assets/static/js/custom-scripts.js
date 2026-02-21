0c6808a3fc5e832c0f3ccbcf5095d0faca469f1e
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2026-02-20 23:40:11 -0800

    Update site to set date for Artemis II ticket release (#102)

--- assets/static/js/custom-scripts.js
+++ assets/static/js/custom-scripts.js
@@ -21,8 +21,8 @@ https://opensource.org/licenses/MIT
     //
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
-        missionName: "NASA/SpaceX Crew-12",
-        launchAt: 1770977700, // the UNIX timestamp of the projected T-0 time
+        missionName: "NASA Artemis II",
+        launchAt: 1772846940, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: false,
     };
     // const missionOverride = null;
