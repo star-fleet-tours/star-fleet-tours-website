22b126b1784c725c4da74f3d61eb962b053326c7
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2026-02-21 13:40:44 -0800

    Update site for Artemis 2 launch delay to NET April (#103)

--- assets/static/js/custom-scripts.js
+++ assets/static/js/custom-scripts.js
@@ -22,7 +22,7 @@ https://opensource.org/licenses/MIT
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
         missionName: "NASA Artemis II",
-        launchAt: 1772846940, // the UNIX timestamp of the projected T-0 time
+        launchAt: 0, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: false,
     };
     // const missionOverride = null;
