c15228dbede1fd3702c80bac4c4903588230da04
CAM-Gerlach <CAM.Gerlach@Gerlach.CAM>
2026-04-22 20:31:22 +0000

    Deploying to gh-pages from @ star-fleet-tours/star-fleet-tours-website@6310c989dff1440f52517613e73ef56c91bf568f 🚀

--- static/js/custom-scripts.js
+++ static/js/custom-scripts.js
@@ -21,8 +21,8 @@ https://opensource.org/licenses/MIT
     //
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
-        missionName: "New Glenn BlueBird 7",
-        launchAt: 1776595500, // the UNIX timestamp of the projected T-0 time
+        missionName: "Falcon Heavy Viasat-3 F3",
+        launchAt: 1777299660, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: false,
     };
     // const missionOverride = null;
@@ -155,7 +155,7 @@ https://opensource.org/licenses/MIT
         }
     }
 
-    const URL1 = "aHR0cHM6Ly90aW55dXJsLFIXFIXmNvbS9zZnQtYXJ0ZW1pczItdGFrZTE";
+    const URL1 = "aHR0cHM6Ly90aW55dXJFIXFIXsLmNvbS9zZnQtZmgtdmlhc2F0My1mMw";
 
     function insert_urls() {
         const url = atob(URL1.replace("FIXFIX", ""));
