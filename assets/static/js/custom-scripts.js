d83002936f9c99b8d01ba6417b225c73ee7ca9fa
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2026-06-09 23:16:11 -0500

    Update site to initial state for Starlink 10-54 mission

--- assets/static/js/custom-scripts.js
+++ assets/static/js/custom-scripts.js
@@ -21,8 +21,8 @@ https://opensource.org/licenses/MIT
     //
     // Otherwise, specify the absence of a missionOverride:
     const missionOverride = {
-        missionName: "SpaceX Starlink 10-53",
-        launchAt: 1780055520, // the UNIX timestamp of the projected T-0 time
+        missionName: "SpaceX Starlink 10-54",
+        launchAt: 1781267220, // the UNIX timestamp of the projected T-0 time
         limitTwoWeeks: false,
     };
     // const missionOverride = null;
